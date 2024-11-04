b3da3f4213f46806280547e05aff06ee93fb72a7
NastiaSuarez <vipautomatisch@gmail.com>
2024-11-04 22:28:03 +0300

    04/11

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+first = int(input("Введите число: "))
+second = int(input("Введите число: "))
+third = int(input("Введите число: "))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if first == second and second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
