c091845e39a84658e4e2987de71489ff424bf079
NastiaSuarez <vipautomatisch@gmail.com>
2024-11-05 11:48:42 +0300

    05/11

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ first = int(input("Введите число: "))
 second = int(input("Введите число: "))
 third = int(input("Введите число: "))
 
-if first == second and second == third:
+if first == second == third:
     print(3)
 elif first == second or second == third or first == third:
     print(2)
