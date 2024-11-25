6a84c38130513e1be1871b0629f550e46d71d3bf
NastiaSuarez <vipautomatisch@gmail.com>
2024-11-25 21:29:46 +0300

    25/11

--- module2_hard.py
+++ module2_hard.py
@@ -1,15 +1,18 @@
 a = int(input("Введите число: "))
+
+def find_pairs(a):
+    pairs = []
+    for x in range(1, a):
+        for k in range(1, a // x + 1):
+            y = a - x * k
+            if y >= x and a % (x + y) == 0:
+                pairs.append((x, y))
+    return pairs
 if not (3 <= a <= 20):
         print("Значение a должно быть в диапазоне от 3 до 20")
 else:
-    def find_pairs(a):
-        pairs = []
-        for x in range(1, a):
-            for k in range(1, a // x + 1):
-                y = a - x * k
-                if y >= x and a % (x + y) == 0:
-                    pairs.append((x, y))
-        return pairs
+    find_pairs(a)
+
 result = find_pairs(a)
 if result:
     print("Найденные пары:")
